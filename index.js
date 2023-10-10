d1d5385fc8d17c90c39ef2afa21ca28af01b8319
Nicolly Moreira <nicollycarmo@eaportal.org>
2023-10-10 14:25:39 -0300

    recebendo parâmetros na rota

--- index.js
+++ index.js
@@ -5,8 +5,16 @@ const app = express()
 
 const caminhoBase = path.join(__dirname, "templates")
 
+app.get('/usuarios/:id', (requesicao, resposta) => {
+    const id = requesicao.params.id
+
+    console.log(`Acessando os dados do usuário ${id}`)
+
+    resposta.sendFile(`${caminhoBase}/usuarios.html`)
+})
+
 app.get('/', (requicao, resposta) => {
-    resposta.sendFile(`${caminhoBase}/index,html`)
+    resposta.sendFile(`${caminhoBase}/index.html`)
 })
 
 app.listen(3000, () => {
